85d1515987ae934e81c74b930545e31892189703
kubotama <16697573+kubotama@users.noreply.github.com>
2020-04-28 16:32:29 +0900

    GREEN: ファンクションからeventを返す。
    ローカルの検証環境ではリクエスト元のIPアドレスは取得できない。

--- src/components/GlipahUi.vue
+++ src/components/GlipahUi.vue
@@ -1,14 +1,25 @@
 <template>
-  <div>{{ glipahData }}</div>
+  <div></div>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
   name: "GlipahUi",
+  mounted: function() {
+    const url = this.getFunctionUrl(window.location.href);
+    // const res = await axios.get(url);
+    // console.log(JSON.parse(res));
+    axios.get(url).then(response => {
+      console.log(response.data);
+    });
+  },
   methods: {
     getFunctionUrl(pageUrl) {
       const url = new URL(pageUrl);
-      if (url.hostname === "localhost") {
+      // if (url.hostname === "localhost" || url.hostname === "127.0.0.1") {
+      if (url.port == 8080) {
         url.port = 9000;
       }
       url.pathname = ".netlify/functions/ipaddress";
