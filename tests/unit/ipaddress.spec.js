3afa80ba1d9e2c6248e40273b423caa395966d21
kubotama <16697573+kubotama@users.noreply.github.com>
2020-04-30 16:57:13 +0900

    axiosをモックして、ファンクションを実際に呼び出さないように修正した。
    axiosの呼び出し回数、引数、返される値のテストを追加した。
    呼び出し回数をクリアするためにjest.config.jsを修正した。

--- tests/unit/ipaddress.spec.js
+++ tests/unit/ipaddress.spec.js
@@ -1,5 +1,14 @@
+import axios from "axios";
 import { shallowMount } from "@vue/test-utils";
 import GlipahUi from "@/components/GlipahUi.vue";
+
+jest.mock("axios");
+axios.get.mockImplementation(() =>
+  Promise.resolve({
+    status: 200,
+    data: "zz.zz.zz.zz"
+  })
+);
 
 describe("ファンクションのURLを取得する。", () => {
   let wrapper;
@@ -14,5 +23,10 @@ describe("ファンクションのURLを取得する。", () => {
     ${"https://glipah.netlify.app/"} | ${"https://glipah.netlify.app/.netlify/functions/ipaddress"}
   `("$beforeUrl -> $afterUrl", ({ beforeUrl, afterUrl }) => {
     expect(wrapper.vm.getFunctionUrl(beforeUrl)).toBe(afterUrl);
+    expect(axios.get).toBeCalledTimes(1);
+    expect(axios.get).toBeCalledWith(
+      "http://localhost:9000/.netlify/functions/ipaddress"
+    );
+    expect(wrapper.vm.glipahData).toBe("zz.zz.zz.zz");
   });
 });
