471ef520314bc792086dc5c8264f1a2be75d7ecc
kubotama <16697573+kubotama@users.noreply.github.com>
2020-05-01 14:39:24 +0900

    日付のフォーマットを変更した。
    YYYY/MM/DDからYYYY-MM-DDに変更した。

--- tests/unit/ipaddress.spec.js
+++ tests/unit/ipaddress.spec.js
@@ -39,13 +39,15 @@ describe("IPアドレスの履歴の一覧表", () => {
     table = wrapper.find("#ipHistory");
   });
 
+  // TODO: 日時のフォーマットを修正する
+  // YYYY-MM-DD HH:MM:SS
   it("表の行数とヘッダの見出し", () => {
     expect(table.element.rows[0].cells[0].innerHTML).toBe("IPアドレス");
     expect(table.element.rows[0].cells[1].innerHTML).toBe("アクセス日時");
     expect(table.element.rows.length).toBe(2);
     expect(table.element.rows[1].cells[0].innerHTML).toBe("zz.zz.zz.zz");
     expect(table.element.rows[1].cells[1].innerHTML).toBe(
-      "2020/04/30 12:34:56"
+      "2020-04-30 12:34:56"
     );
   });
 });
