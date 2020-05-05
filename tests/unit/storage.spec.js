ac44d01ec6128d7531c2a5a9400293e7138745c4
kubotama <16697573+kubotama@users.noreply.github.com>
2020-05-05 14:11:19 +0900

    RED: mountの後でデータベースが存在するテストを追加した

--- tests/unit/storage.spec.js
+++ tests/unit/storage.spec.js
@@ -1,9 +1,47 @@
+import axios from "axios";
 import Dexie from "dexie";
+import { shallowMount } from "@vue/test-utils";
+import GlipahUi from "@/components/GlipahUi.vue";
+
+global.indexedDB = require("fake-indexeddb");
+global.IDBCursor = require("fake-indexeddb/lib/FDBCursor");
+global.IDBCursorWithValue = require("fake-indexeddb/lib/FDBCursorWithValue");
+global.IDBDatabase = require("fake-indexeddb/lib/FDBDatabase");
+global.IDBFactory = require("fake-indexeddb/lib/FDBFactory");
+global.IDBIndex = require("fake-indexeddb/lib/FDBIndex");
+global.IDBKeyRange = require("fake-indexeddb/lib/FDBKeyRange");
+global.IDBObjectStore = require("fake-indexeddb/lib/FDBObjectStore");
+global.IDBOpenDBRequest = require("fake-indexeddb/lib/FDBOpenDBRequest");
+global.IDBRequest = require("fake-indexeddb/lib/FDBRequest");
+global.IDBTransaction = require("fake-indexeddb/lib/FDBTransaction");
+global.IDBVersionChangeEvent = require("fake-indexeddb/lib/FDBVersionChangeEvent");
+
+jest.mock("axios");
+axios.get.mockImplementation(() =>
+  Promise.resolve({
+    status: 200,
+    data: { "client-ip": "zz.zz.zz.zz" }
+  })
+);
+
+const OriginalDate = Date;
+const dateToUse = new Date("2020-04-30 12:34:56");
+jest.spyOn(global, "Date").mockImplementation(arg => {
+  return arg ? new OriginalDate(arg) : dateToUse;
+});
+Date.now = jest.fn(() => OriginalDate.now());
 
 describe("IPアドレスの履歴データベースを確認する。", () => {
-  it("データベースが存在しないことを確認する。", () => {
-    Dexie.exists("Glipah").then(exists => {
-      expect(exists).toBeFalsy();
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallowMount(GlipahUi);
+  });
+
+  it("データベースが存在することを確認する。", async () => {
+    await Dexie.exists("Glipah").then(exists => {
+      expect(exists).toBeTruthy();
     });
+    expect(wrapper.isVueInstance()).toBeTruthy();
   });
 });
