b0f4c7182fa4f5e6c7f4c83595f8c7a4025fc991
kubotama <16697573+kubotama@users.noreply.github.com>
2020-05-05 14:10:28 +0900

    indexeddbのテストのために、fake-indexeddbをインストールした
    jest.config.jsに設定を追加した。

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   preset: "@vue/cli-plugin-unit-jest",
   clearMocks: true,
-  testURL: "http://localhost:8080"
+  testURL: "http://localhost:8080/",
+  setupFiles: ["fake-indexeddb/auto"]
 };
