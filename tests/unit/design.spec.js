fbba3924609d2ec634582d007c635180855007f7
kubotama <16697573+kubotama@users.noreply.github.com>
2020-05-05 14:21:55 +0900

    GREEN: タイトルの取得するテストに成功する。

--- tests/unit/design.spec.js
+++ tests/unit/design.spec.js
@@ -3,10 +3,16 @@ import cheerio from "cheerio";
 
 describe("画面のデザインを改善する。 #11", () => {
   it("タイトルをGLIPAH: Global IP Address Historyに変更する。", async () => {
-    const response = await axios.get("http://localhost:8080");
-    const body = response.data;
-    const $ = cheerio.load(body);
-    const title = $("title").text();
-    expect(title).toBe("GLIPAH: Global IP Address History");
+    // const response = await axios.get("http://localhost:8080");
+    // const body = response.data;
+    // const $ = cheerio.load(body);
+    // const title = $("title").text();
+    // expect(title).toBe("GLIPAH: Global IP Address History");
+    await axios.get("http://localhost:8080").then(response => {
+      const body = response.data;
+      const $ = cheerio.load(body);
+      const title = $("title").text();
+      expect(title).toBe("GLIPAH: Global IP Address History");
+    });
   });
 });
