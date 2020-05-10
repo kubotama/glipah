697d35b934e0745137046725fd1d7e1d0581b709
kubotama <16697573+kubotama@users.noreply.github.com>
2020-05-10 21:48:06 +0900

    保存されているデータが表示されているテストを追加した。
    後から追加したデータが上に表示される。

--- tests/unit/storage.spec.js
+++ tests/unit/storage.spec.js
@@ -181,4 +181,33 @@ describe("2回めのアクセス(違うIPアドレス)のテスト", () => {
         });
       });
   });
+
+  it("保存されているデータが表示されていることを確認する", done => {
+    db.access
+      .clear()
+      .then(() => {
+        dateToUse = new Date("2020-05-6 01:02:03");
+        ipToUse = "ab.cd.ef.gh";
+        return wrapper.vm.accessFunction();
+      })
+      .then(() => {
+        dateToUse = new Date("2020-05-10 00:11:22");
+        ipToUse = "11.22.33.44";
+        wrapper.vm.accessFunction().then(() => {
+          const table = wrapper.find("#ipHistory");
+          expect(table.element.rows.length).toBe(3);
+          expect(table.element.rows[0].cells[1].innerHTML).toBe("IPアドレス");
+          expect(table.element.rows[0].cells[2].innerHTML).toBe("アクセス日時");
+          expect(table.element.rows[1].cells[1].innerHTML).toBe("11.22.33.44");
+          expect(table.element.rows[1].cells[2].innerHTML).toBe(
+            "2020-05-10 00:11:22"
+          );
+          expect(table.element.rows[2].cells[1].innerHTML).toBe("ab.cd.ef.gh");
+          expect(table.element.rows[2].cells[2].innerHTML).toBe(
+            "2020-05-06 01:02:03"
+          );
+          done();
+        });
+      });
+  });
 });
