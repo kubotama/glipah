da63368645fd17213108bf62d2736a06eb472945
kubotama <16697573+kubotama@users.noreply.github.com>
2020-05-26 10:54:33 +0900

    Jestで警告が表示されない設定

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,5 @@ module.exports = {
   preset: "@vue/cli-plugin-unit-jest",
   clearMocks: true,
   testURL: "http://localhost:8080/",
-  setupFiles: ["fake-indexeddb/auto"]
+  setupFiles: ["fake-indexeddb/auto", "./tests/unit/setup.js"]
 };
