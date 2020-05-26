ea983ba259e65b914f30b88a3ddeb253c2e6881f
kubotama <16697573+kubotama@users.noreply.github.com>
2020-05-26 22:29:50 +0900

    ボタンを右下に配置する
    v-btnを右寄せにするために、上位のdivのclassにtext-rightを割り当てている。

--- src/components/GlipahUi.vue
+++ src/components/GlipahUi.vue
@@ -22,44 +22,47 @@
         </tbody>
       </table>
     </div>
-    <v-dialog v-model="dialog" width="500">
-      <template v-slot:activator="{ on }">
-        <v-btn color="black" dark v-on="on" outlined>
-          プライバシーポリシー
-        </v-btn>
-      </template>
-
-      <v-card>
-        <v-card-title class="headline grey lighten-2" primary-title>
-          プライバシーポリシー
-        </v-card-title>
-
-        <v-card-text>
-          当サイトでは、Googleによるアクセス解析ツール「Googleアナリティクス」を使用しています。このGoogleアナリティクスはデータの収集のためにCookieを使用しています。このデータは匿名で収集されており、個人を特定するものではありません。
-          この機能はCookieを無効にすることで収集を拒否することが出来ますので、お使いのブラウザの設定をご確認ください。
-          この規約に関しての詳細は<a
-            href="https://marketingplatform.google.com/about/analytics/terms/jp/"
-            target="”_blank”"
-          >
-            Googleアナリティクスサービス利用規約のページ </a
-          >や
-          <a
-            href="https://policies.google.com/technologies/ads?hl=ja"
-            target="”_blank”"
-            >Googleポリシーと規約ページ</a
-          >をご覧ください。
-        </v-card-text>
-
-        <v-divider></v-divider>
-
-        <v-card-actions>
-          <v-spacer></v-spacer>
-          <v-btn color="primary" text @click="dialog = false">
-            閉じる
+    <hr class="border-line" />
+    <div class="text-right dialog-privacy">
+      <v-dialog v-model="dialog" width="500">
+        <template v-slot:activator="{ on }">
+          <v-btn color="black" dark v-on="on" outlined id="buttonPrivacy">
+            プライバシーポリシー
           </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-dialog>
+        </template>
+
+        <v-card>
+          <v-card-title class="headline grey lighten-2" primary-title>
+            プライバシーポリシー
+          </v-card-title>
+
+          <v-card-text>
+            当サイトでは、Googleによるアクセス解析ツール「Googleアナリティクス」を使用しています。このGoogleアナリティクスはデータの収集のためにCookieを使用しています。このデータは匿名で収集されており、個人を特定するものではありません。
+            この機能はCookieを無効にすることで収集を拒否することが出来ますので、お使いのブラウザの設定をご確認ください。
+            この規約に関しての詳細は<a
+              href="https://marketingplatform.google.com/about/analytics/terms/jp/"
+              target="”_blank”"
+            >
+              Googleアナリティクスサービス利用規約のページ </a
+            >や
+            <a
+              href="https://policies.google.com/technologies/ads?hl=ja"
+              target="”_blank”"
+              >Googleポリシーと規約ページ</a
+            >をご覧ください。
+          </v-card-text>
+
+          <v-divider></v-divider>
+
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn color="primary" text @click="dialog = false">
+              閉じる
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+    </div>
   </div>
 </template>
 
@@ -205,5 +208,18 @@ h1 {
 #buttonClick {
   border-style: solid;
   padding: 2pt;
+}
+#buttonPrivacy {
+  margin-left: auto;
+  margin-right: 0px;
+}
+.border-line {
+  margin-top: 10pt;
+  margin-bottom: 10pt;
+}
+
+.dialog-privacy {
+  margin-right: 10pt;
+  margin-bottom: 10pt;
 }
 </style>
