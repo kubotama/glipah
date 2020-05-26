82f21755990835b2c34859d702737f0a4717e856
kubotama <16697573+kubotama@users.noreply.github.com>
2020-05-26 10:59:46 +0900

    design.spec.jsが成功

--- tests/unit/design.spec.js
+++ tests/unit/design.spec.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import cheerio from "cheerio";
 
-describe.skip("画面のデザインを改善する。 #11", () => {
+describe("画面のデザインを改善する。 #11", () => {
   it("タイトルがGLIPAH: Global IP Address Historyであることを確認する。", async () => {
     await axios.get("http://localhost:8080").then(response => {
       expect(
