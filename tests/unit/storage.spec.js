71bebb8eed0bdcdcbda58349f982b1d551f978ab
kubotama <16697573+kubotama@users.noreply.github.com>
2020-06-06 15:32:35 +0900

    テストケース6を追加した
    既に変更された分で対応されているため、プログラムの変更は不要だった。

--- tests/unit/storage.spec.js
+++ tests/unit/storage.spec.js
@@ -286,4 +286,43 @@ describe("2回めのアクセス(違うIPアドレス)のテスト", () => {
         });
       });
   });
+
+  it("3回とも同じIPアドレスからのアクセス、日時の順にアクセスされています。: テストケース6", done => {
+    db.access
+      .clear()
+      .then(() => {
+        dateToUse = new Date("2020-05-6 01:02:03");
+        ipToUse = "ab.cd.ef.gh";
+        return wrapper.vm.accessFunction();
+      })
+      .then(() => {
+        dateToUse = new Date("2020-05-21 07:23:39");
+        return wrapper.vm.accessFunction();
+      })
+      .then(() => {
+        dateToUse = new Date("2020-06-01 22:11:00");
+        wrapper.vm.accessFunction().then(() => {
+          const table = wrapper.find("#ipHistory");
+          expect(table.element.rows.length).toBe(2);
+          expect(table.element.rows[0].cells.length).toBe(4);
+          expect(table.element.rows[0].cells[0].innerHTML).toBe("IPアドレス");
+          expect(table.element.rows[0].cells[1].innerHTML).toBe("アクセス回数");
+          expect(table.element.rows[0].cells[2].innerHTML).toBe(
+            "初回のアクセス日時"
+          );
+          expect(table.element.rows[0].cells[3].innerHTML).toBe(
+            "最新のアクセス日時"
+          );
+          expect(table.element.rows[1].cells[0].innerHTML).toBe("ab.cd.ef.gh");
+          expect(table.element.rows[1].cells[1].innerHTML).toBe("3");
+          expect(table.element.rows[1].cells[2].innerHTML).toBe(
+            "2020-05-06 01:02:03"
+          );
+          expect(table.element.rows[1].cells[3].innerHTML).toBe(
+            "2020-06-01 22:11:00"
+          );
+          done();
+        });
+      });
+  });
 });
